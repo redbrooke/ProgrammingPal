9be477ca76a632fa787c0c739eca61c5a0998cb4
NootRoot <NootRoot@DESKTOP-LPVUJEM>
2020-11-05 16:47:44 +0000

    Started adding canvas draw commands

---
+++ ProgrammingPal/DrawHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingPal
+{
+    class DrawHandler
+    {
+        Graphics graphics;
+        Pen pen;
+        int Xpos;
+        int Ypos;
+
+        public DrawHandler(Graphics graphics)
+        {
+            this.graphics = graphics;
+            Xpos = 0;
+            Ypos = 0;
+            pen = new Pen(Color.Black, 1);
+        }
+
+        public void DrawLine(int Xdest, int Ydest) 
+        {
+            graphics.DrawLine(pen, Xpos, Ypos, Xdest, Ydest);
+            Xpos = Xdest;
+            Ypos = Ydest;
+        }
+
+        public void DrawSquare(int width)
+        {
+            graphics.DrawRectangle(pen, Xpos, Ypos, Xpos + width, Ypos + width);
+        }
+    }
+}
